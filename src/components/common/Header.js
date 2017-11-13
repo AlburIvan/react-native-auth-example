dece87bd7f7a919bff823e5e995652503ddd4725
AlburIvan <albur.ivan@outlook.com>
2017-11-13 00:06:52 -0400

    Add new components & Modify as per lectures

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -20,9 +20,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         height: 60,
         paddingTop: 15,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
+        // shadowColor: '#000',
+        // shadowOffset: { width: 0, height: 2 },
+        // shadowOpacity: 0.2,
         elevation: 2,
         position: 'relative'
     },
